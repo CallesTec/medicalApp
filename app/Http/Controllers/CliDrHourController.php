ca39e5b1ed53b3e64d6ff0981df227681df6b4cf
Fabio Menjivar <menjivarfabio79@gmail.com>
2022-12-21 10:01:47 -0600

    CliDrHourController Creation

---
+++ app/Http/Controllers/CliDrHourController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CliDrHour;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CliDrHourController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //EC: Get all schedules
+        $cdhours = CliDrHour::all();
+        return response()->json([
+            "results" => $cdhours
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //EC:  Store a new shedule
+        $cdhour = new CliDrHour();
+        $cdhour->clinic_id = $request->clinic_id;
+        $cdhour->doctor_id = $request->doctor_id;
+        $cdhour->startHour = $request->startHour;
+        $cdhour->endHour = $request->endHour;
+
+        if ($cdhour != null) {
+            $cdhour->save();
+            return $cdhour;
+        } 
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //FM: Get a schedule by id
+        $cdhour = CliDrHour::findOrFail($id);
+        return $cdhour;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //EC:  Store a new shedule
+        $cdhour = CliDrHour::findOrFail($id);
+        $cdhour->clinic_id = $request->clinic_id;
+        $cdhour->doctor_id = $request->doctor_id;
+        $cdhour->startHour = $request->startHour;
+        $cdhour->endHour = $request->endHour;
+
+        if ($cdhour != null) {
+            $cdhour->save();
+            return $cdhour;
+        } 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //EC: Delete a schedule by id
+        $cdhour = CliDrHour::destroy($id);
+        return response()->json([
+            "message" => "An schedule has been deleted",
+            "result" => $cdhour
+        ], Response::HTTP_OK);
+    }
+}
