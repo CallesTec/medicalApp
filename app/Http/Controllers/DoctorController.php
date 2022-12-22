be7a15d730f2a279e5cfc693c352a286decf7955
Fabio Menjivar <menjivarfabio79@gmail.com>
2022-12-21 20:45:26 -0600

    Cleaning the code

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Doctor;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DoctorController extends Controller
 {
--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Patient;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class PatientController extends Controller
 {
